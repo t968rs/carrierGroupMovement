c5185a9449929d9bcc4af3586f9ead17db0576a2
t968rs <kevtg1212@gmail.com>
2024-07-09 15:34:08 -0600

    Added routes

--- src/popupContent.js
+++ src/popupContent.js
@@ -16,3 +16,21 @@ export function createCarrierPointPopupContent(clickedfeature, alias_mapping)
     }
     return popupContent;
 }
+
+export function createRouteLinePopupContent(clickedfeature, alias_mapping)
+{
+    let popupContent = '<ul>';
+    for (let property in clickedfeature.properties)
+    {
+        // Use alias if available, otherwise use the original property name
+        const aliasProperty = alias_mapping[property] || property;
+        if (!["info_src", "cur_miss"].includes(property)) {
+            popupContent += `<li><strong>${aliasProperty}</strong>: ${clickedfeature.properties[property]}</li>`
+        }
+        else if (property === "cur_miss") {
+            let shorterText = clickedfeature.properties[property].substring(0, 42)
+            popupContent += `<li><strong>${aliasProperty}</strong>: ${shorterText}</li>`
+        }
+    }
+    return popupContent;
+}
