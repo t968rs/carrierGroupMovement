737faeba1c7cacf7b626e855cbcc840a1f971e48
t968rs <kevtg1212@gmail.com>
2024-07-08 23:01:32 -0600

    new icons

--- src/globe_functions.js
+++ src/globe_functions.js
@@ -21,3 +21,39 @@ function spinGlobe(map, spinEnabled, userInteracting, maxSpinZoom, slowSpinZoom,
             map.easeTo({ center, duration: 1000, easing: (n) => n });
         }
     }
+
+// Function to draw route
+function drawRoute(map, pastPoint, currentPoint, futurePoint) {
+    const route = {
+        type: 'Feature',
+        geometry: {
+            type: 'LineString',
+            coordinates: [pastPoint.geometry.coordinates, currentPoint.geometry.coordinates, futurePoint.geometry.coordinates]
+        }
+    };
+
+    if (map.getSource('route')) {
+        map.getSource('route').setData(route);
+    } else {
+        map.addSource('route', {
+            type: 'geojson',
+            data: route
+        });
+
+        map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: 'route',
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            paint: {
+                'line-color': '#888',
+                'line-width': 8
+            }
+        });
+    }
+}
+
+export { drawRoute };
