fe8c2310001cf271e761182e5585a5944ba7ff4e
t968rs <kevtg1212@gmail.com>
2024-07-09 14:39:37 -0600

    Added routes~!

--- src/popupContent.js
+++ src/popupContent.js
@@ -1,18 +1,18 @@
 // Function to create popup content
-    export function createPopupContent(clickedfeature)
+export function createCarrierPointPopupContent(clickedfeature)
+{
+    let popupContent = '<ul>';
+    for (let property in clickedfeature.properties)
     {
-        let popupContent = '<ul>';
-        for (let property in clickedfeature.properties)
-        {
-            // Use alias if available, otherwise use the original property name
-            const aliasProperty = aliasMapping[property] || property;
-            if (!["info_src", "cur_miss"].includes(property)) {
-                popupContent += `<li><strong>${aliasProperty}</strong>: ${clickedfeature.properties[property]}</li>`
-            }
-            else if (property === "cur_miss") {
-                let shorterText = clickedfeature.properties[property].substring(0, 42)
-                popupContent += `<li><strong>${aliasProperty}</strong>: ${shorterText}</li>`
-            }
+        // Use alias if available, otherwise use the original property name
+        const aliasProperty = aliasMapping[property] || property;
+        if (!["info_src", "cur_miss"].includes(property)) {
+            popupContent += `<li><strong>${aliasProperty}</strong>: ${clickedfeature.properties[property]}</li>`
         }
-        return popupContent;
+        else if (property === "cur_miss") {
+            let shorterText = clickedfeature.properties[property].substring(0, 42)
+            popupContent += `<li><strong>${aliasProperty}</strong>: ${shorterText}</li>`
+        }
     }
+    return popupContent;
+}
