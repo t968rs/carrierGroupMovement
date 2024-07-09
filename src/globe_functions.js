fc81b543fe84f5e38d4561324d8550d509bd6295
t968rs <kevtg1212@gmail.com>
2024-07-08 23:40:41 -0600

    new icons

--- src/globe_functions.js
+++ src/globe_functions.js
@@ -1,27 +1,4 @@
-function spinGlobe(map, spinEnabled, userInteracting, maxSpinZoom, slowSpinZoom, secondsPerRevolution) {
-        // If the map is not defined, log an error and return.
-        if (!map) {
-            console.error(map, " is undefined");
-            return;
-        }
-        const zoom = map.getZoom();
-        console.log("Current Zoom: ", zoom)
-        if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
-            let distancePerSecond = 360 / secondsPerRevolution;
-            if (zoom > slowSpinZoom) {
-                // Slow spinning at higher zooms
-                const zoomDif =
-                    (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
-                distancePerSecond *= zoomDif;
-            }
-            const center = map.getCenter();
-            center.lng -= distancePerSecond;
-            // Smoothly animate the map over one second.
-            // When this animation is complete, it calls a 'moveend' event.
-            map.easeTo({ center, duration: 1000, easing: (n) => n });
-        }
-    }
-export { spinGlobe };
+
 
 // Function to draw route
 function drawRoute(map, pastPoint, currentPoint, futurePoint) {
